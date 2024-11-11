be5e34a56025c3111ffe1492842d1a6c4a7c3018
Abdullahahmad666 <l201326@lhr.nu.edu.pk>
2024-11-11 19:53:25 +0500

    Animations

--- app/src/main/java/com/example/dotaskforme/SplashScreen.java
+++ app/src/main/java/com/example/dotaskforme/SplashScreen.java
@@ -3,7 +3,10 @@ package com.example.dotaskforme;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreen extends AppCompatActivity {
     private ImageView ivLogo;
+    TextView tvslogan;
+
     @Override
 
 
@@ -28,6 +33,13 @@ public class SplashScreen extends AppCompatActivity {
 
         });
         ivLogo = findViewById(R.id.ivlogo);
+        tvslogan = findViewById(R.id.tvslogan);
+        Animation logoAnimation = AnimationUtils.loadAnimation(this, R.anim.logo_animation);
+        Animation textAnimation = AnimationUtils.loadAnimation(this, R.anim.text_animation);
+
+
+        ivLogo.startAnimation(logoAnimation);
+        tvslogan.startAnimation(textAnimation);
 
         new Handler().postDelayed(() -> {
             // Start the next activity with ActivityResultLauncher
