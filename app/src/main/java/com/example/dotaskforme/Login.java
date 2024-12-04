0ca1ef7fb3982ea20597ec1932ad3ecf4db3077e
Abdullahahmad666 <l201326@lhr.nu.edu.pk>
2024-12-04 20:29:24 +0500

    forgot password implemented using firebase

--- app/src/main/java/com/example/dotaskforme/Login.java
+++ app/src/main/java/com/example/dotaskforme/Login.java
@@ -1,10 +1,15 @@
 package com.example.dotaskforme;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +17,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Login extends AppCompatActivity {
-    TextView gotosignup;
+    TextView gotosignup,forgot_password;
     Button signin;
+    FirebaseAuth auth;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +36,9 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
 
-        gotosignup = findViewById(R.id.gotosignup);
-        signin = findViewById(R.id.loginbtn);
+
         gotosignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,5 +55,40 @@ public class Login extends AppCompatActivity {
                 finish();
             }
         });
+        forgot_password.setOnClickListener(view -> {
+            EditText etEmail = new EditText(view.getContext());
+            AlertDialog.Builder forgetPasswordDialog = new AlertDialog.Builder(view.getContext())
+                    .setTitle("Forget Password Email...")
+                    .setView(etEmail)
+                    .setPositiveButton("Send", (dialogInterface, i) -> {
+                        String email = etEmail.getText().toString().trim();
+                        if(TextUtils.isEmpty(email)) {
+                            etEmail.setError("Give valid email address");
+                        } else {
+                            ProgressDialog progressDialog = new ProgressDialog(Login.this);
+                            progressDialog.show();
+                            auth.sendPasswordResetEmail(email)
+                                    .addOnCompleteListener(task -> {
+                                        if(task.isSuccessful()) {
+                                            Toast.makeText(Login.this, "Check your inbox...", Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(Login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                        progressDialog.dismiss();
+                                    });
+                        }
+                    })
+                    .setNegativeButton("Cancel", null);
+            forgetPasswordDialog.show();
+        });
     }
+    public void init()
+    {
+        gotosignup = findViewById(R.id.gotosignup);
+        signin = findViewById(R.id.loginbtn);
+        forgot_password = findViewById(R.id.forgot_password);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+    }
+
 }
